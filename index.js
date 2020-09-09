45123c62621a408f7b8d95e7c77648aeb09d857e
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-09 06:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,3 @@ var unleashDog = function(dogName="Byron", dogBreed="poodle") {
 
 const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
-function exerciseDog(dog, breed) {
-  return routine.map(fn => fn(dog, breed))
-}
